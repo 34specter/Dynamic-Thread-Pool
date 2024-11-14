8c48669cd8a9382efdfd03a76f40ae4932ba2a06
孙悟饭 <2187757396@qq.com>
2024-11-14 15:16:47 +0800

    feat:02:采集线程池配置数据

---
+++ dynamic-thread-pool-starter/src/main/java/cn/shidu/middleware/dynamic/thread/pool/sdk/domain/model/entity/ThreadPoolConfigEntity.java
@@ -0,0 +1,11 @@
+package cn.shidu.middleware.dynamic.thread.pool.sdk.domain.model;
+
+/*
+ *@title ThreadPoolConfigEntity
+ *@description
+ *@author 孙悟饭
+ *@version 1.0
+ *@create 2024/11/14 11:40
+ */
+public class ThreadPoolConfigEntity {
+}
